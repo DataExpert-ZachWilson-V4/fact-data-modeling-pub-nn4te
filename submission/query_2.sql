84f7c0ac89c7389736b237f5106fecded9c264ca
nn4te <64181464+nn4te@users.noreply.github.com>
2024-06-03 21:15:29 +1000

    Update query_2.sql

---
+++ submission/query_2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE nattyd.user_devices_cumulated (
+    user_id BIGINT,
+    dates_active MAP(VARCHAR,ARRAY(DATE)),
+    date DATE
+)
+WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['date']
+)
